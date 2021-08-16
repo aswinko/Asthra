13cdb78874cd5c45302fd8ccb552dde1f4327862
AsthraGfx <88666700+AsthraGfx@users.noreply.github.com>
2021-08-16 19:52:43 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -211,13 +211,16 @@
         else{
             $("#back-to-top").stop().animate({ bottom:'-50px' },300,'easeInOutCubic')
         }
-	    
-	 if($(window).scrollTop() > 200){
+
+ 	if($(window).scrollTop() > 200){
         $("#Onam").hide()        
         } 
         else{
             $("#Onam").show()
         }
+
+	
+
     });
     /* -------------------
     Preloader
